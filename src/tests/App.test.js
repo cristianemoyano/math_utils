14dd044d90c4ba2945b4710a04565e3fee7e2408
cmoyano <cristianmoyano.mza@gmail.com>
2020-10-10 21:20:28 -0300

    Add factorial util

--- src/tests/App.test.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import Home from '../pages/Home';
-
-test('Todo', () => {
-  const { getByDisplayValue } = render(<Home />);
-
-});
---
+++ src/tests/factorial.test.js
@@ -0,0 +1,42 @@
+import React from 'react';
+
+import {factorial} from '../math/factorial';
+
+
+it('Factorial of a integer number', () => {
+	expect(factorial(1)).toEqual(1);
+	expect(factorial(2)).toEqual(2);
+	expect(factorial(3)).toEqual(6);
+	expect(factorial(4)).toEqual(24);
+	expect(factorial(5)).toEqual(120);
+	expect(factorial(6)).toEqual(720);
+});
+
+it('Factorial of a float number', () => {
+	//The value is casted to int.
+	expect(factorial(1.2)).toEqual(1);
+	expect(factorial(2.2)).toEqual(2);
+	expect(factorial(3.2)).toEqual(6);
+	expect(factorial(4.2)).toEqual(24);
+	expect(factorial(5.2)).toEqual(120);
+	expect(factorial(6.2)).toEqual(720);
+});
+
+it('Factorial of a string number', () => {
+	//The value is casted to int.
+	expect(factorial("1")).toEqual(1);
+	expect(factorial("2")).toEqual(2);
+	expect(factorial("3")).toEqual(6);
+	expect(factorial("4")).toEqual(24);
+	expect(factorial("5")).toEqual(120);
+	expect(factorial("6")).toEqual(720);
+});
+
+
+it('Factorial of a null value', () => {
+	expect(factorial(null)).toEqual(null);
+});
+
+it('Factorial of a undefined value', () => {
+	expect(factorial(undefined)).toEqual(null);
+});
